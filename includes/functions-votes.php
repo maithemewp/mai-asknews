f66344404281248628d5222c84be48861ccc5ee5
JiveDig <jivetheuniversal@gmail.com>
2024-09-18 13:19:36 -0400

    hide prediction on outcomes for logged out users

--- includes/functions-votes.php
+++ includes/functions-votes.php
@@ -219,7 +219,7 @@ function maiasknews_get_singular_vote_box() {
  */
 function maiasknews_get_outcome_box( $data ) {
 	$html       = '';
-	$prediction = maiasknews_get_vote_elements( 'prediction' );
+	$prediction = $data['show_prediction'] ? maiasknews_get_vote_elements( 'prediction' ) : '';
 	$selected   = maiasknews_get_vote_elements( 'selected' );
 	$status     = maiasknews_get_vote_elements( 'winner' );
 	$home_class = $data['winner_home'] ? 'winner' : 'loser';
